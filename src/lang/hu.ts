6b43d4cdaae95d567e7e94aa8d530e9145429964
Gömböcz Zsolt <gombocz.zsolt@fws.hu>
2025-10-13 19:45:33 +0200

    chore(welcome-screen): Add translations

--- src/lang/hu.ts
+++ src/lang/hu.ts
@@ -6,5 +6,11 @@ export default {
 		backToHome: "Vissza a főoldalra",
 		version: "Verzió",
 		errorOccured: "Váratlan hiba történt! ({{error}})",
+		welcome: {
+			welcomeToRescanny: "Üdv a Rescanny-ben",
+			loginOrRegister: "Lépj be a fiókodba vagy hozz létre egy újat",
+			loginButton: "Bejelentkezés",
+			registerButton: "Regisztráció",
+		},
 	},
 };
