afd72c5915bd70b3ef9fae11c094d59149061616
Gömböcz Zsolt <gombocz.zsolt@fws.hu>
2025-10-21 20:20:02 +0200

    chore: Remove unused imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,16 @@
 import i18n from "i18next";
-import { lazy, StrictMode } from "react";
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router";
+import { ToastContainer } from "react-toastify";
 import en from "./lang/en.ts";
 import hu from "./lang/hu.ts";
-
-const _Welcome = lazy(() => import("./ui/screens/welcome.tsx"));
-const _NotFound = lazy(() => import("./ui/screens/not-found.tsx"));
-
-import { ToastContainer } from "react-toastify";
 import { useApiStatusStore } from "./store/api-status.ts";
 
 import "./index.css";
 import { initReactI18next } from "react-i18next";
 
-import router from "./routes.tsx";
+import { router } from "./routes.tsx";
 
 const root = document.getElementById("root");
 
