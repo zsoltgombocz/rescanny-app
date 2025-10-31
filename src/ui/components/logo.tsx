604df7152d03bd7f59f2afe638411f8fb18975c9
Gömböcz Zsolt <gombocz.zsolt@fws.hu>
2025-10-31 20:43:42 +0100

    feat: Add size prop to app logo component

--- src/ui/components/logo.tsx
+++ src/ui/components/logo.tsx
@@ -2,9 +2,19 @@ import type * as React from "react";
 import type { JSX } from "react";
 import { twMerge } from "tailwind-merge";
 
-export function Logo({
-	className,
-}: React.HTMLProps<HTMLDivElement>): JSX.Element {
+type Props = React.HTMLProps<HTMLDivElement> & {
+	scale?: "big" | "small";
+};
+
+export function Logo({ className, scale = "big" }: Props): JSX.Element {
+	const sizeClass = twMerge(
+		scale === "big" && "w-16 h-16 sm:w-20 sm:h-20 rounded-2xl",
+		scale === "small" && "w-10 h-10 sm:w-14 sm:h-14 rounded-xl",
+	);
+	const fontSizeClass = twMerge(
+		scale === "big" && "text-xl sm:text-2xl",
+		scale === "small" && "text-lg sm:text-xl",
+	);
 	return (
 		<div
 			className={twMerge(
@@ -18,14 +28,16 @@ export function Logo({
 				}
 			></div>
 			<div
-				className={
-					"relative w-16 h-16 sm:w-20 sm:h-20 rounded-2xl bg-gradient-to-r from-indigo-500 to-teal-500 flex items-center justify-center shadow-2xl"
-				}
+				className={twMerge(
+					sizeClass,
+					"relative bg-gradient-to-r from-indigo-500 to-teal-500 flex items-center justify-center shadow-2xl",
+				)}
 			>
 				<span
-					className={
-						"text-xl sm:text-2xl font-semibold tracking-tight text-white"
-					}
+					className={twMerge(
+						fontSizeClass,
+						"font-semibold tracking-tight text-white",
+					)}
 				>
 					R
 				</span>
