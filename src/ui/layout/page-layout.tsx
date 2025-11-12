7b6de075f1876cd437ae421665c518e6e209f9a3
Gömböcz Zsolt <gombocz.zsolt@fws.hu>
2025-11-12 14:04:35 +0100

    feat: Add page layout type

--- src/ui/layout/page-layout.tsx
+++ src/ui/layout/page-layout.tsx
@@ -1,13 +1,25 @@
 import type { ComponentProps, JSX } from "react";
+import { twMerge } from "tailwind-merge";
+
+type Props = ComponentProps<"section"> & {
+	type?: "centered" | "simple";
+};
 
 export default function PageLayout({
 	children,
-}: ComponentProps<"section">): JSX.Element {
+	className,
+	type = "centered",
+}: Props): JSX.Element {
 	return (
 		<section
-			className={
-				"w-full h-full flex-grow flex flex-col justify-around items-center"
-			}
+			className={twMerge(
+				"w-full h-full",
+				type === "centered" &&
+					"flex-grow flex flex-col justify-around items-center",
+				type === "simple" &&
+					"overflow-y-auto overflow-x-hidden pt-6 pb-32 max-w-2xl mx-auto",
+				className,
+			)}
 		>
 			{children}
 		</section>
