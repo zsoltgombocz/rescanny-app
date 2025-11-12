f7cc9a4df7697e70f7386d20912e3997eea0a5f9
Gömböcz Zsolt <gombocz.zsolt@fws.hu>
2025-11-12 14:04:50 +0100

    wip: Home page

---
+++ src/ui/screens/home.tsx
@@ -0,0 +1,11 @@
+import type { JSX } from "react";
+import Link from "../components/link";
+import PageLayout from "../layout/page-layout";
+
+export default function Home(): JSX.Element {
+	return (
+		<PageLayout>
+			Home
+		</PageLayout>
+	);
+}
