f12d24e89bd261353e2e2743c9c7d0c7cafe576a
Gömböcz Zsolt <gombocz.zsolt@fws.hu>
2025-11-12 14:05:19 +0100

    chore: Biome format

--- src/ui/screens/home.tsx
+++ src/ui/screens/home.tsx
@@ -1,11 +1,6 @@
 import type { JSX } from "react";
-import Link from "../components/link";
 import PageLayout from "../layout/page-layout";
 
 export default function Home(): JSX.Element {
-	return (
-		<PageLayout>
-			Home
-		</PageLayout>
-	);
+	return <PageLayout>Home</PageLayout>;
 }
