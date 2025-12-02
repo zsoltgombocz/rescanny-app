9f03e8bccdff8be349d594cfb8526f56fc9f10c8
Gömböcz Zsolt <gombocz.zsolt@fws.hu>
2025-12-02 20:53:51 +0100

    fix: Do not try to fetch user in root loader if using magic link login

--- src/loaders/root-loader.tsx
+++ src/loaders/root-loader.tsx
@@ -1,9 +1,10 @@
 import i18next from "i18next";
+import type { LoaderFunctionArgs } from "react-router";
 import { get as httpGet } from "../api/http";
 import { useApiStatusStore } from "../store/api-status";
 import { type User, userGetState } from "../store/user";
 
-export async function rootLoader() {
+export async function rootLoader({ request }: LoaderFunctionArgs) {
 	const { hydrated, hydrate } = userGetState();
 
 	await useApiStatusStore.getState().fetch();
@@ -11,6 +12,12 @@ export async function rootLoader() {
 	const apiStatusPayload = useApiStatusStore.getState();
 
 	const maintenanceMode = apiStatusPayload.data?.maintenance;
+
+	const url = new URL(request.url);
+
+	if (url.pathname.startsWith("/login/magic-link")) {
+		return { user: null };
+	}
 
 	if (!hydrated && typeof window !== "undefined") {
 		const me = await httpGet("/user/me").catch(() => null);
